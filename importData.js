a9fe71ffd8099d1e6608d0962544aa6561d06efe
alexa-whitney <alexawhitney@gmail.com>
2023-05-03 12:24:11 -0600

    Seeded Firebase database with characters and quotes!!

--- importData.js
+++ importData.js
@@ -1,41 +1,53 @@
-const Character = require('./models/Character');
-const Quote = require('./models/Quote');
-const charactersData = require('./data/characters.json');
-const quotesData = require('./data/quotes.json');
+const admin = require('firebase-admin');
+const serviceAccount = require('./src/config/schitty-api-firebase-key.json');
+const fs = require('fs');
+const charactersData = JSON.parse(fs.readFileSync('data/characters.json', 'utf8'));
+const quotesData = JSON.parse(fs.readFileSync('data/quotes.json', 'utf8'));
 
-// Connect to the MongoDB database
-mongoose.connect('mongodb://localhost/schitty-api', { useNewUrlParser: true, useUnifiedTopology: true });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
-// Import characters and quotes
-async function importData() {
-  try {
-    // Import characters
-    const characters = await Character.insertMany(charactersData);
+const db = admin.firestore();
 
-    // Iterate over quotes data
-    for (const quoteData of quotesData) {
-      // Find the associated character
-      const character = characters.find(char => char.name === quoteData.characterName);
+// Delete existing data in order to reseed:
+async function deleteAllCharacters() {
+    const charactersSnapshot = await db.collection('characters').get();
+    const batch = db.batch();
+  
+    charactersSnapshot.docs.forEach((doc) => {
+      batch.delete(doc.ref);
+    });
+  
+    await batch.commit();
+    console.log('Deleted all characters.');
+}
+  
 
-      // Create a new quote with the characterId
-      const quote = new Quote({
-        characterId: character._id,
-        text: quoteData.text,
-        season: quoteData.season,
-        episode: quoteData.episode
-      });
 
-      // Save the quote to the database
-      await quote.save();
+// Add characters to Firebase DB:
+async function addCharacters(charactersData) {
+    const charactersRef = db.collection('characters');
+
+    for (const character of charactersData) {
+        const characterDoc = await charactersRef.add(character);
+        console.log(`Added character ${character.name} with ID: ${characterDoc.id}`);
     }
-
-    console.log('Data import complete');
-  } catch (err) {
-    console.error('Error importing data:', err);
-  } finally {
-    // Close the database connection
-    mongoose.connection.close();
-  }
 }
 
-importData();
+// Add quotes to Firebase DB:
+async function addQuotes(quotesData) {
+    const quotesRef = db.collection('quotes');
+
+    for (const quote of quotesData) {
+        const quoteDoc = await quotesRef.add(quote);
+        console.log(`Added quote "${quote.text}" with ID: ${quoteDoc.id}`);
+    }
+}
+
+// Call the functions to populate the Firebase DB:
+(async () => {
+    await deleteAllCharacters();
+    await addCharacters(charactersData);
+    await addQuotes(quotesData);
+})();
